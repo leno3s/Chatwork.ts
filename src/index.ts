cede8af94672b0a711e4275fdc953a7538d49a1d
leno3s <3so.d.na@gmail.com>
2021-05-17 19:23:17 +0900

    indexの追加

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { Client } from "./Client";
+import { HttpRequestGAS } from "./HttpRequestGAS";
+import { HttpRequestNode } from "./HttpRequestNode";
+import { Room } from "./Room";
+
+export { Client, HttpRequestGAS, HttpRequestNode, Room };
