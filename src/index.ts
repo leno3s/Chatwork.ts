20dafb304fab131e1f2ee57c08662bab381bccbd
leno3s <3so.d.na@gmail.com>
2021-06-05 04:23:00 +0900

    indexへのMessage.tsの追加

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Client } from "./Client";
 import { HttpRequestGAS } from "./HttpRequestGAS";
 import { HttpRequestNode } from "./HttpRequestNode";
 import { Room } from "./Room";
+import { Message } from "./Message";
 import { Chatwork } from "./types/Chatwork";
 
-export { Client, HttpRequestGAS, HttpRequestNode, Room, Chatwork };
+export { Client, HttpRequestGAS, HttpRequestNode, Room, Message, Chatwork };
