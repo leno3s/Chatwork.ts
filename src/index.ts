e1bccc79a41536005ea26e49b46bf2d120e310e4
leno3s <3so.d.na@gmail.com>
2021-06-05 19:04:29 +0900

    indexの修正

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,20 @@ import { HttpRequestNode } from "./HttpRequestNode";
 import { Room } from "./Room";
 import { Message } from "./Message";
 import { Task } from "./Task";
+import { MyTask } from "./MyTask";
+import { RoomTask } from "./RoomTask";
 import { RequestOfContact } from "./RequestOfContact";
-import { Chatwork } from "./Types";
+import * as Types from "./Types";
 
 export {
   Client,
   HttpRequestGAS,
   HttpRequestNode,
   Room,
+  MyTask,
+  RoomTask,
   Message,
   Task,
   RequestOfContact,
-  Chatwork,
+  Types,
 };
